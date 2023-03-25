9c87e6a149260022b9e5ed66684cb82908b6e223
Aleksandr Riabov <ardeks16@gmail.com>
2023-03-25 19:10:32 +0300

    Add button

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/ARdekS/homework-qs
+
+go 1.20
--- main.go
+++ main.go
@@ -4,8 +4,9 @@ import (
 	"encoding/json"
 	"fmt"
 	"html/template"
-	"io/ioutil"
+	"io"
 	"net/http"
+	"sort"
 )
 
 // type Todo struct {
@@ -17,7 +18,7 @@ type Node struct {
 	Text   string `json:"text"`
 	Parent int    `json:"parent"`
 	ID     int    `json:"id"`
-	Index  int
+	Hide   bool
 	Nodes  []*Node
 }
 
@@ -54,11 +55,13 @@ func initTree() *Node {
 	}
 	data.Nodes[2].addNew()
 	data.Nodes[1].Nodes[2].addNew()
+	data.Nodes[1].Nodes[1].addNew()
+	data.Nodes[1].Nodes[1].Nodes[0].addNew()
 	return &data
 }
 
 type Cache struct {
-	Storage map[int]*Node
+	Cache map[int]*Node
 }
 
 type ID struct {
@@ -66,13 +69,39 @@ type ID struct {
 }
 
 func NewCache() *Cache {
-	return &Cache{Storage: make(map[int]*Node, 0)}
+	return &Cache{Cache: make(map[int]*Node, 0)}
 }
 func (c *Cache) addItem(n *Node) {
-	c.Storage[n.ID] = n
-	if p, ok := c.Storage[n.Parent]; ok {
-		p.Nodes = append(p.Nodes, n)
+	if _, ok := c.Cache[n.ID]; !ok {
+		//если n это чей либо родитель то мы должны добавить детей и скрыть их
+		for _, i := range c.Cache {
+			if i.Parent == n.ID {
+				n.Nodes = append(n.Nodes, i)
+				i.Hide = true
+			}
+		}
+		//если родитель для n уже есть то мы должны добавить ему ребенка n
+		if p, ok := c.Cache[n.Parent]; ok {
+			n.Hide = true
+			p.Nodes = append(p.Nodes, n)
+		}
+
+		c.Cache[n.ID] = n
+
 	}
+}
+func (c *Cache) returnItems() []*Node {
+
+	var result []*Node = make([]*Node, 0, 1)
+	for _, v := range c.Cache {
+		if !v.Hide {
+			result = append(result, v)
+		}
+	}
+	sort.SliceStable(result, func(i, j int) bool {
+		return result[i].ID < result[j].ID
+	})
+	return result
 }
 
 var cache = NewCache()
@@ -81,7 +110,7 @@ func addRequest(w http.ResponseWriter, r *http.Request) {
 	// Проверяем метод запроса
 	if r.Method == http.MethodPost {
 		// Читаем тело (body) запроса
-		body, err := ioutil.ReadAll(r.Body)
+		body, err := io.ReadAll(r.Body)
 		if err != nil {
 
 			// Обработка ошибки чтения тела (body) запроса
@@ -93,7 +122,7 @@ func addRequest(w http.ResponseWriter, r *http.Request) {
 		err = json.Unmarshal(body, &node)
 		// fmt.Println(node)
 		cache.addItem(&node)
-		fmt.Println(cache)
+		fmt.Println(cache.returnItems())
 		if err != nil {
 			// Обработка ошибки распаковки JSON
 			http.Error(w, "Error decoding JSON", http.StatusBadRequest)
@@ -104,23 +133,24 @@ func addRequest(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
 		return
 	}
-	tmpl := template.Must(template.ParseFiles("cache.html"))
-	tmpl.Execute(w, cache)
+	tmpl := template.Must(template.ParseFiles("cache.html", "item.html"))
+
+	tmpl.Execute(w, cache.returnItems())
 }
 
 var data *Node
 
-type foo struct {
-	Storage *Node
-	Cache   *Cache
-}
+// type foo struct {
+// 	Storage *Node
+// 	Cache   *Cache
+// }
 
 func main() {
 	data = initTree()
 
 	// Обходим дерево
 	traverse(data)
-	tmpl := template.Must(template.ParseFiles("base.html", "item.html", "cache.html"))
+	tmpl := template.Must(template.ParseFiles("base.html", "item.html"))
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		tmpl.Execute(w, data)
 	})
